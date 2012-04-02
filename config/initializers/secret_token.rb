c30d0d3cd6a1464e2f5a29954ee58902dcb44bcc
Justin <roninnanaki@me.com>
2012-04-02 15:49:23 -0600

    project start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThoughtLocker::Application.config.secret_token = '6e9f270f202db6fddca6d2c47faf517e5027fc123b9eb1b86708ec09122f5a9894546b17af7bd92ef280c53e71586e38085de1f979f9c0831f959c5f54df2882'
