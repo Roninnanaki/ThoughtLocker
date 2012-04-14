a8f0d3953f821823a519d9e678343bf74fc4b281
Justin <roninnanaki@me.com>
2012-04-13 20:12:25 -0600

    completed secure signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
+  def show
+  	@user = User.find(params[:id])
+  end
+
   def new
+  	@user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    #REGEX_FOR_GETTING_NAME = /.*?(?=[@])/
+    @user.name = @user.email[/(.*?)(?=[@])/, 1]
+    
+    if @user.save
+      flash[:success] = "Welcome to the ThoughtLocker!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ThoughtLocker::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root :to => 'static_pages#login'
   
