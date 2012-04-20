66e9b3400cb76b973f630d3beba5d7fe9ec295a8
Tyler Garlick <tjgarlick@gmail.com>
2012-04-20 14:18:06 -0600

    added attr_accessor, fixed a problem

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,5 @@
 class Blog < ActiveRecord::Base
-	attr_accessible :name
+	attr_accessible :name, :tags_attributes
 	
 	validates :user_id, presence: true
 	validates :name, presence: true, :length => { :maximum => 100 }
@@ -7,8 +7,7 @@ class Blog < ActiveRecord::Base
 	belongs_to :user
 	has_many :tags, dependent: :destroy
 	
-	accepts_nested_attributes_for :tags, :allow_destroy => true, 
-		:reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
+	accepts_nested_attributes_for :tags, :allow_destroy => true
 	
 	default_scope order: 'blogs.created_at DESC'
 end
